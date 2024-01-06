dcfc5b9c49599631802f27be0caa4d4153a87435
dtanguyet <73488112+dtanguyet@users.noreply.github.com>
2024-01-06 09:43:37 +0700

    Add files via upload

---
+++ Common/GeneralModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class GeneralModel
+    {
+        public class ResponeseMessage
+        {
+            public bool IsSuccess { get; set; }
+            public int Status { get; set; }
+            public string Message { get; set; }
+            public object Data { get; set; }
+        }
+
+        public class UserLoginRequest
+        {
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class UserRegisterRequest
+        {
+
+            public string Email { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public string ConfirmPassword { get; set; }
+
+        }
+    }
+}
---
+++ Common/Helpers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+using System.Linq;
+
+namespace Common
+{
+    public class Helpers
+    {
+        public static string[] arrHexa = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+        public static string HasPassword(string password)
+        {
+            //chuyển mật khẩu sang mảng byte
+            var sha1 = new SHA1CryptoServiceProvider();
+            var bytesPw = Encoding.ASCII.GetBytes(password);
+
+            //salt và chuyển salt sang byte
+            var salt = DateTime.Now.Millisecond.ToString();
+            var bytesSalt = Encoding.ASCII.GetBytes(salt);
+
+            //hash
+            var bytesPwhashed = sha1.ComputeHash(bytesPw);
+
+            //gắn salt lưu xuống database
+            var bytesResult = new byte[bytesPwhashed.Length + bytesSalt.Length];
+            Array.Copy(bytesPwhashed, bytesResult, bytesPwhashed.Length);
+            Array.Copy(bytesSalt, 0, bytesResult, bytesPwhashed.Length, bytesSalt.Length);
+
+
+            // chuyển array byte sang string
+            var strPwHash = ArrByteToString(bytesResult);
+            return strPwHash;
+        }
+
+        public static string ArrByteToString(Byte[] arrByte)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in arrByte)
+            {
+                //get 4 bit first
+                sb.Append(Helpers.arrHexa[(b >> 4)]);
+                //get 4 bit second
+                sb.Append(Helpers.arrHexa[(b & 15)]);
+            }
+
+            return sb.ToString();
+        }
+
+        public static byte[] StringToByteArray(string str)
+        {
+            return Enumerable.Range(0, str.Length)
+                             .Where(x => x % 2 == 0)
+                             .Select(x => Convert.ToByte(str.Substring(x, 2), 16))
+                             .ToArray();
+        }
+    }
+}
